bab48c5843c87d846888975e96773a5730be5d74
Satoshi SAKAO <ottijp@users.noreply.github.com>
2024-09-05 20:48:57 +0900

    add: support rule for provider namespace

--- test/rule-engine.spec.ts
+++ test/rule-engine.spec.ts
@@ -24,4 +24,15 @@ describe('rule-engine', () => {
       '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg1/providers/Microsoft.Storage/storageAccounts/testmodify1',
     ])
   })
+
+  it("filter provider namespace", async () => {
+    const input = await import ('./rule-engine-testdata-providernamespace.json') as OperationResult
+
+    const actual = denoiseOperationResult(input, [{
+      providerNamespace: 'Microsoft.Storage',
+    }])
+    assert.deepEqual(actual.changes.map(c => c.resourceId), [
+      '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg2/providers/Microsoft.OperationalInsights/workspaces/testmodify2',
+    ])
+  })
 })
