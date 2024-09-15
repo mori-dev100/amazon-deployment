94198d02984907e550bc267771ff34e00a9a87d1
Satoshi SAKAO <ottijp@users.noreply.github.com>
2024-09-15 17:52:45 +0900

    add: basic command line parsing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import { parseCommand } from '@/command'
 import { readOperationResult, readConfig } from '@/reader'
 import { printPretty } from '@/writer'
 import { denoiseOperationResult } from '@/rule-engine'
 
 class App {
   async run() {
+    await parseCommand()
+
     const config = await readConfig('./az-deployment-denoise.json')
     const result = await readOperationResult()
     const denoisedResult = denoiseOperationResult(result, config.rules)
