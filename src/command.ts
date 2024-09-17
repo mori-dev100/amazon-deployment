c2b6c65c611893cda1130b4e68bd776fb26a6265
Satoshi SAKAO <ottijp@users.noreply.github.com>
2024-09-17 16:47:15 +0900

    add: support YAML config file #2

--- src/command.ts
+++ src/command.ts
@@ -1,6 +1,22 @@
+import * as fs from 'node:fs/promises'
 import { Command } from 'commander'
 import { readPackageInfo } from '@/reader'
 import { CommandOption } from '@/types/command-option'
+
+/**
+ * Check if the file exists.
+ * @param path - file path to check
+ * @return true if the file exists
+ */
+async function fileExists(path: string) {
+  try {
+    await fs.stat(path)
+    return true
+  }
+  catch {
+    return false
+  }
+}
 
 /**
  * Parse CLI command line arguments.
@@ -16,13 +32,25 @@ export async function parseCommand(): Promise<CommandOption> {
   program.version(packageInfo.version)
 
   // set options
-  program.option('-f, --config-file <file>', 'config file')
+  program.option('-f, --config-file <file>', 'config file (JSON/YAML)')
 
   program.parse()
+  const options = program.opts<CommandOption>()
+
+  // specify config file
+  const configJsonFile = './az-deployment-denoise.json'
+  const configYamlFile = './az-deployment-denoise.yml'
+  const configJsonExists = await fileExists(configJsonFile)
+  const configYamlExists = await fileExists(configYamlFile)
+  let configFile = options.configFile
+  if (!configFile && configJsonExists) {
+    configFile = configJsonFile
+  }
+  else if (!configFile && configYamlExists) {
+    configFile = configYamlFile
+  }
 
   // assemble and return command options
-  const options = program.opts<CommandOption>()
-  const configFile = options.configFile || './az-deployment-denoise.json'
   return {
     configFile,
   }
